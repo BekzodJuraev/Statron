a1cad42371890fd7c8ef8dc96e2bee99e2042c1f
Bekzod <powerzver98@gmail.com>
2024-01-05 16:37:57 +0500

    started

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     username=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     phone_number = PhoneNumberField()
     first_name=models.CharField(max_length=150)
-    last_name=models.CharField(max_length=150)
+    last_name=models.CharField(max_length=140)
     email=models.EmailField()
     balance = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     currency=models.CharField(max_length=10)
