8376cf8b60d0916a7ac171812445c677c9334764
Bekzod <powerzver98@gmail.com>
2024-04-05 14:46:16 +0500

    added newbase

--- Statron/settings.py
+++ Statron/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'Statron.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'postgres',
+    #    'USER': 'postgres',
+     #   'PASSWORD': '123',
+      #  'HOST': 'localhost',
+       # 'PORT': '5432',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
